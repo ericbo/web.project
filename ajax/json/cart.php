2d672b8ad076d52c2dcc191bc5dd82395591c8fa
ericbom <eeb1010@gmail.com>
2015-12-02 17:07:01 -0500

    Added UNTESTED cart login for AJAX requests.

--- ajax/json/cart.php
+++ ajax/json/cart.php
@@ -1,2 +1,40 @@
 <?php
+session_start();
 header('Content-type:application/json;charset=utf-8');
+/*
+* This file expects the following post variables: itemID, qty.
+*/
+
+if(isset($_POST['itemID']) && isset($_POST['qty'])) {
+	updateCart($_POST['itemID'], $_POST['qty'])
+}
+else
+	echo json_encode(displayCart());
+
+
+function displayCart() {
+	if(isset($_SESSION['cart']))
+		return $_SESSION['cart'];
+	return array();
+}
+
+function updateCart($itemID, $qty) {
+	//Update the sessions cart
+	if(isset($_SESSION['cart']))
+	{
+		//Check if item is already in the cart, and get its key.
+		$key = array_search($itemID, array_column($_SESSION['cart'], $itemID));
+		if($key)
+		{
+			if($qty < 0)
+				array_plice($_SESSION['cart'], $key, 1);
+			else
+				if($qty <= 255)
+					$_SESSION['cart']['key']['qty'] = $qty;
+
+		}
+	}
+	else
+		if($qty <= 255 && $qty >= 0) 
+			array_push($_SESSION['cart'], array("itemID"=>$itemID, "qty"=>$qty));
+}
